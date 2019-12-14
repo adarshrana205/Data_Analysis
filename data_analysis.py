035b8344bf1f963de82d965df76838e6d899963d
Adarsh Rana <adarshrana205@gmail.com>
2019-12-14 21:27:30 +0530

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -11,13 +11,16 @@ import numpy as np
 path="https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 
 headers=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-df=pd.read_csv(path,header=None)
+df=pd.read_csv(path,header=None) #import data	
+# To export data we use "to_csv" method
 df.columns=headers
 print(df.head(5))
 print(df.dtypes)
-print(df.describe(include="all"))
+print(df.describe(include="all")) # check statiscal summary
 df["a"]=df["a"]+1
 print(df.head(5))
+
+# How to deal with missing values in data
 df.dropna(subset=["price"],axis=0,inplace=True)
 mean = df["a"].mean() 
 df["a"].replace(np.nan, mean)
@@ -32,3 +35,6 @@ df["b"]=(df["b"]-df["b"].mean())/df["b"].std()   # Z-score method
 bins=np.linspace(min(df["b"]),max(df["b"]),4)
 group_names=["Low","Medium","High"]
 df["b-binned"]=pd.cut(df["b"],bins,labels=group_names,include_lowest=True)
+
+#Turning categorical variables into quantitative variables
+pd.get_dummies(df['d'])
