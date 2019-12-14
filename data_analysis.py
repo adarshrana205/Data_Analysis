fa50b7341298beb6f333aa8b651bdfa349b9a023
Adarsh Rana <adarshrana205@gmail.com>
2019-12-14 14:12:59 +0530

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -11,13 +11,16 @@ import numpy as np
 path="https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 
 headers=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-df=pd.read_csv(path,header=None)
+df=pd.read_csv(path,header=None) #import data
+# To export data we use "to_csv" method
 df.columns=headers
 print(df.head(5))
 print(df.dtypes)
-print(df.describe(include="all"))
+print(df.describe(include="all")) #check statistical summary
 df["a"]=df["a"]+1
 print(df.head(5))
+
+# How to deal with missing values in data
 df.dropna(subset=["price"],axis=0,inplace=True)
 mean = df["a"].mean() 
 df["a"].replace(np.nan, mean)
