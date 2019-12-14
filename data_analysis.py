5863505b42e1a06b002fe8a672789171beb8edc3
Adarsh Rana <adarshrana205@gmail.com>
2019-12-14 21:05:40 +0530

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -11,16 +11,13 @@ import numpy as np
 path="https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 
 headers=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-df=pd.read_csv(path,header=None) #import data
-# To export data we use "to_csv" method
+df=pd.read_csv(path,header=None)
 df.columns=headers
 print(df.head(5))
 print(df.dtypes)
-print(df.describe(include="all")) #check statistical summary
+print(df.describe(include="all"))
 df["a"]=df["a"]+1
 print(df.head(5))
-
-# How to deal with missing values in data
 df.dropna(subset=["price"],axis=0,inplace=True)
 mean = df["a"].mean() 
 df["a"].replace(np.nan, mean)
@@ -30,3 +27,8 @@ df["a"].replace(np.nan, mean)
 df["b"]=df["b"]/df["b"].max() # Simple feature scaling method
 df["b"]=(df["b"]-df["b"].min())/(df["b"].max()-df["b"].min()) # Min-max method
 df["b"]=(df["b"]-df["b"].mean())/df["b"].std()   # Z-score method
+
+#Data Binning
+bins=np.linspace(min(df["b"]),max(df["b"]),4)
+group_names=["Low","Medium","High"]
+df["b-binned"]=pd.cut(df["b"],bins,labels=group_names,include_lowest=True)
